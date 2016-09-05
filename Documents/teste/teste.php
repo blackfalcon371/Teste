1978709b63eea0a5e4fd6f36a4feda00cddf74df
Lucas <lucaspaz_93@hotmail.com>
2016-09-05 18:18:04 -0300

    teste começo site

---
+++ Documents/teste/teste.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST["submit"])){
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$database = "teste";
+
+try {
+    $pdo = new PDO("mysql:host=$servidor;dbname=$database", $usuario, $senha);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO testando (nome)
+    VALUES ('".$_POST["nome"]."')";
+
+    $pdo->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+}
+
+$pdo = null;
+?>
---
+++ Documents/teste/teste.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `teste`.`testando` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`id`));
